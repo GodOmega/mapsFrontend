d5447b293ba336769a91abd0ee9c6a33e944dfeb
= <=>
2022-07-27 12:40:52 -0400

    adding pwa

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png" />
+          <meta name="theme-color" color="#fff" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
-          <title>T1EMPO APP</title>
         </Head>
         <body>
           <Main />
